bb7e27af359d4eb9208a0e4cb290b8e302c860b7
Seth-Joseph <alencherry@hotmail.com>
2025-06-20 04:27:47 +0000

    [automatic commit] 2025-06-20 04:27:46 UTC 104.28.162.206

--- part1/app.js
+++ part1/app.js
@@ -41,11 +41,10 @@ async function initDatabase() {
     // Seed walk requests
     await db.query(`
       INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-       ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),
-       '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-
-  ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), 
-   '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')),
+        '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),
+        '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 
   ((SELECT dog_id FROM Dogs WHERE name = 'Sagar' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), 
    '2025-06-11 10:00:00', 60, 'City Square', 'open'),
