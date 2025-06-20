0248f6dc4f5fffb5ec0c83a22993a41d772a5c2a
Seth-Joseph <alencherry@hotmail.com>
2025-06-20 07:07:48 +0000

    [automatic commit] 2025-06-20 07:07:47 UTC 104.28.162.206

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -59,4 +59,15 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// Logout route
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+    res.clearCookie('connect.sid');
+    res.json({ message: 'Logged out' });
+  });
+});
+
 module.exports = router;
