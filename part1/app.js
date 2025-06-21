115424eaf1883e053f092c3cd2539a12f94bf404
Seth-Joseph <alencherry@hotmail.com>
2025-06-21 02:04:21 +0000

    [automatic commit] 2025-06-21 02:04:20 UTC 104.28.243.79

--- part1/app.js
+++ part1/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const pool = require('./db');
-const initializeDatabase = require('./initdb');
 
 var app = express();
 
@@ -18,16 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// Initialize DB and insert test data on startup
-(async () => {
-  try {
-    await initializeDatabase();
-    console.log('Database initialized with test data.');
-  } catch (err) {
-    console.error('Database initialization failed:', err);
-  }
-})();
 
 // Helper to get a connection
 async function getConnection() {
